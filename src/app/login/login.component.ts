4b05ddfc9ab3d35a47a3df6d9ffc84a03ac38076
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-07-27 08:47:19 -0700

    user service created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {Route, Router} from '@angular/router';
+import {UserServiceClient} from '../services/user.service.client';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    username;
+    password;
+    login(username, password) {
+        console.log([username, password]);
+        this.service
+            .login(username, password)
+            .then(() => {
+                this.router.navigate(['profile']);
+            });
+    }
 
-  ngOnInit() {
-  }
+    constructor(private router: Router,
+                private service: UserServiceClient) { }
 
+    ngOnInit() {
+    }
 }
